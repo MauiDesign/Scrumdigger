1966df5a214a102d2283aa7e16ddd3df7a87dd4a
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2024-03-15 18:44:12 +1300

    Display attendees in the edit view

--- Scrumdigger/Views/DetailEditView.swift
+++ Scrumdigger/Views/DetailEditView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailEditView: View {
     @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -22,10 +23,38 @@ struct DetailEditView: View {
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
             }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
         }
     }
 }
 
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
+/*
 #Preview {
     DetailEditView()
 }
+*/
