a23e36d3a8d1d7a6e62d3c664692121cb7912bf7
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2024-04-04 15:25:06 +1300

    Passing data with bindings - Add a theme view

---
+++ Scrumdigger/Views/ThemeView.swift
@@ -0,0 +1,32 @@
+//
+//  ThemeView.swift
+//  Scrumdigger
+//
+//  Created by Aroha Design on 4/04/24.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme:. buttercup)
+}
+/**
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView()
+    }
+}
+**/
