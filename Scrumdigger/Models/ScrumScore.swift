f60b5a1cae154ba6093fc983fbb0667450bc7d58
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2024-04-07 15:57:21 +1200

    add a method to load data

--- Scrumdigger/Models/ScrumScore.swift
+++ Scrumdigger/Models/ScrumScore.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@MainActor
 class ScrumStore: ObservableObject {
     @Published var scrums: [DailyScrum] = []
     
@@ -14,6 +15,19 @@ class ScrumStore: ObservableObject {
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             .appendingPathComponent("scrums.data")
+    }
+    
+    func load() async throws {
+        let task = Task<[DailyScrum], Error> {
+            let fileURL = try Self.fileURL()
+            guard let data = try? Data(contentsOf: fileURL) else {
+                return []
+            }
+            let dailScrums = try JSONDecoder().decode([DailyScrum].self, from: data)
+            return dailScrums
+        }
+        let scrums = try await task.value
+        self.scrums = scrums
     }
 }
 
