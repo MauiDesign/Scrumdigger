75294641298ed9cdd3f2b18a4ae9e7b3112e6023
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2024-04-07 16:06:19 +1200

    load data on app launch

--- Scrumdigger/ScrumdiggerApp.swift
+++ Scrumdigger/ScrumdiggerApp.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 @main
 struct ScrumdiggerApp: App {
-    @State private var scrums = DailyScrum.sampleData
+    //@State private var scrums = DailyScrum.sampleData // hard code data
+    @StateObject private var store = ScrumStore() // load data
     
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $scrums)
+            ScrumsView(scrums: $store.scrums)
+                .task {
+                    do {
+                        try await store.load()
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
         }
     }
 }
