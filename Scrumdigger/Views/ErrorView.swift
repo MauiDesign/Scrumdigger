f8145ac57074741600d4180d3c47ffc71d74c174
MauiDesign <36174695+MauiDesign@users.noreply.github.com>
2024-04-13 16:28:24 +1200

    creating an error view

--- Scrumdigger/Views/ErrorView.swift
+++ Scrumdigger/Views/ErrorView.swift
@@ -11,16 +11,50 @@ struct ErrorView: View {
     let errorWrapper: ErrorWrapper
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Text("An error has occured")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+        
     }
 }
 
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
+
+/**
 #Preview {
     enum SampleError: Error {
     case errorRequired
     }
     
-    static var 
+    var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+    }
     
-    ErrorView()
+    ErrorView(errorWrapper: wrapper)
 }
+*/
